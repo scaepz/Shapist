a3fe62153267a18a3a3a906c4a47d5c42810e215
scaepz <jonathan.svahn@live.se>
2016-01-29 15:35:39 +0200

    WriteRow, ReadRow

--- Level editor/FileReader.cpp
+++ Level editor/FileReader.cpp
@@ -26,7 +26,7 @@ BOOL CFileReader::ReadFile2(LPCSTR file, TCHAR * &content)
 
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
-		return false;
+		return FALSE;
 	}
 	dwFileSize = GetFileSize(hFile, NULL);
 
@@ -40,18 +40,56 @@ BOOL CFileReader::ReadFile2(LPCSTR file, TCHAR * &content)
 	if (ReadFile(hFile, content, dwFileSize, &dwBytesRead, NULL))
 	{
 		CloseHandle(hFile);
-		return true;
+		return TRUE;
 	}
 	else
 	{
 		CloseHandle(hFile);
-		return false;
+		return FALSE;
 	}
 
 }
-void CFileReader::WriteNewRowToFile(LPCSTR file, TCHAR * &content)
+BOOL CFileReader::WriteRow(LPCSTR file, TCHAR * &content)
 {
+	HANDLE hFile;
+	DWORD dwFileSize = 0;
+	TCHAR newRow[2] = { '\r', '\n' };
 
+	hFile = CreateFile(file,               // file to open
+		GENERIC_WRITE,          // open for write
+		FILE_SHARE_WRITE,       // share for writing
+		NULL,                  // default security
+		OPEN_ALWAYS,         // creates new if does not exist
+		FILE_ATTRIBUTE_NORMAL, // normal file
+		NULL);                 // no attr. template
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		return FALSE;
+	}
+
+	//Move cursor to end of file
+	SetFilePointer(hFile, GetFileSize(hFile, NULL), NULL, FILE_BEGIN);
+
+	DWORD dwBytesWritten;
+	if (WriteFile(hFile, newRow, 2, &dwBytesWritten, NULL))
+	{
+		if (WriteFile(hFile, content, strlen(content), &dwBytesWritten, NULL))
+		{
+			CloseHandle(hFile);
+			return TRUE;
+		}
+		else
+		{
+			CloseHandle(hFile);
+			return FALSE;
+		}
+	}
+	else
+	{
+		CloseHandle(hFile);
+		return FALSE;
+	}
 }
 BOOL CFileReader::ReplaceFileContent(LPCSTR file, TCHAR * &content)
 {
@@ -62,25 +100,25 @@ BOOL CFileReader::ReplaceFileContent(LPCSTR file, TCHAR * &content)
 		GENERIC_WRITE,          // open for write
 		FILE_SHARE_WRITE,       // share for writing
 		NULL,                  // default security
-		CREATE_ALWAYS,         // creates new if does not exist
+		CREATE_ALWAYS,         // creates always -> replace old file
 		FILE_ATTRIBUTE_NORMAL, // normal file
 		NULL);                 // no attr. template
 
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
-		return false;
+		return FALSE;
 	}
 
 	DWORD dwBytesWritten;
 	if (WriteFile(hFile, content, strlen(content), &dwBytesWritten, NULL))
 	{
 		CloseHandle(hFile);
-		return true;
+		return TRUE;
 	}
 	else
 	{
 		CloseHandle(hFile);
-		return false;
+		return FALSE;
 	}
 }
 
@@ -95,5 +133,50 @@ BOOL CFileReader::ReplaceString(TCHAR *& str, LPSTR newContent)
 	{
 		str[i] = newContent[i];
 	}
-	return true;
+	return TRUE;
+}
+
+BOOL CFileReader::ReadRow(LPCSTR filepath, TCHAR *& file, int row)
+{
+	if (!ReadFile2(filepath, file))
+	{
+		return false;
+	}
+	int currentRow = 0;
+	BOOL foundRow = FALSE;
+	TCHAR * result = new TCHAR[512];
+	int currentRowIndex = 0;
+
+	for (int i = 0; i < strlen(file); i++)
+	{
+		if (currentRow == row)
+		{
+			foundRow = TRUE;
+			if (file[i] != '\n')
+			{
+				result[currentRowIndex] = file[i];
+				currentRowIndex++;
+			}
+		}
+		if (file[i] == '\n') currentRow++;
+		if (currentRow > row) break;
+	}
+	if (foundRow == TRUE)
+	{
+		result[currentRowIndex] = '\0';
+		delete file;
+
+		//make new string instead of result because it is unnecessarily long
+		file = new TCHAR[strlen(result)];
+		for (int i = 0; i < strlen(result); i++)
+		{
+			file[i] = result[i];
+		}
+		file[strlen(result)] = '\0';
+
+		delete result;
+
+		return TRUE;
+	}
+	else return FALSE;
 }
--- Level editor/FileReader.h
+++ Level editor/FileReader.h
@@ -4,7 +4,8 @@ class CFileReader
 {
 public:
 	BOOL ReadFile2(LPCSTR filepath, TCHAR *&);
-	void WriteNewRowToFile(LPCSTR file, TCHAR *& text);
+	BOOL ReadRow(LPCSTR filepath, TCHAR *&, int row);
+	BOOL WriteRow(LPCSTR file, TCHAR *& text);
 	BOOL ReplaceFileContent(LPCSTR file, TCHAR *& text);
 	BOOL ReplaceString(TCHAR *&, LPSTR newContent);
 	CFileReader();
